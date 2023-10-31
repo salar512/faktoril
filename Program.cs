51fb9a7c0af1376cf3d593e29793ebf5d96471ee
Hani <Hani@DESKTOP-Q9D08B7>
2023-10-31 21:24:26 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktoril
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//سالار رحیمی ،فاکتوریل
+            Console.Write(" number: ");
+            int number = int.Parse(Console.ReadLine());
+            int fact = 1, n = 1;
+            while (n <= number)
+            {
+                fact *= n;
+                n++;
+            }
+            Console.WriteLine("\n{0}! = {1}\n\n", number, fact);
+            Console.ReadKey( );
+        }
+    }
+}
